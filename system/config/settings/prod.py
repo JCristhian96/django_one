08a804e6f2e8f7d5ec0ed7096ef1f0e0f2cb5466
Juan Cristhian <63811492+JCristhian96@users.noreply.github.com>
2022-08-19 16:31:58 -0500

    Change ALLOWED

--- system/config/settings/prod.py
+++ system/config/settings/prod.py
@@ -5,7 +5,7 @@ from . import db
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["www.jc-django.tk", "jc-django.tk"]
 
 
 # Database
